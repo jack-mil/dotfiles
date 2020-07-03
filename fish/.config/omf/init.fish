315321b60d714fa48c050f3c383a8120b2c39824
jackmil.rpi4 <62065280+jack-mil@users.noreply.github.com>
2020-07-03 10:42:06 -0400

    small fish changes

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -6,10 +6,11 @@ set -g theme_color_scheme gruvbox
 set -g theme_nerd_fonts yes
 
 set -g theme_title_display_process yes
-set -g theme_title_use_abbreviated_path no 
+set -g theme_title_display_path no
 
 set -g theme_date_timezone America/New_York
 set -g theme_date_format "+%l:%M %p"
 
-set -g fish_prompt_pwd_dir_length 3
+set -g fish_prompt_pwd_dir_length 8
+set -g theme_newline_cursor yes
 
