98de8b2dcb9809c30efcb3c2e66e1da2b5534b03
jack-mil <62065280+jack-mil@users.noreply.github.com>
2024-09-15 13:17:12 -0400

    fix post_deploy on windows

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -1,5 +1,5 @@
 {{#if (eq dotter.os "windows")}}
-echo "Deployed on Windows: {{doter.packages}}"
+echo "Deployed on Windows: {{{#each dotter.packages}}{{#if this}}{{@key}} {{/if}}{{/each}}"
 {{else}}
 echo "Deployed on Linux: {{#each dotter.packages}}{{#if this}}{{@key}} {{/if}}{{/each}}"
 {{/if}}
