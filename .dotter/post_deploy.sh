c3a64a52d076f27b886c063ed5c4df8d438e9990
jack-mil <62065280+jack-mil@users.noreply.github.com>
2024-09-15 11:13:08 -0400

    remove managing allowed_signers with post_deploy (buggy)

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -1,9 +1,5 @@
-{{#if (and dotter.packages.git signing_key)}}
-    {{#if (eq dotter.os "windows")}}
-@echo off
-for /f "tokens=*" %%j in ('type {{replace (replace signing_key "~" "%USERPROFILE%") "/" "\\"}}') do set SSH_KEY=%%j
-echo {{email}} namespaces="git" %SSH_KEY% >> %USERPROFILE%\.ssh\allowed_signers
-    {{else}}
-echo "{{email}} namespaces=\"git\" $(cat {{signing_key}})" >> ~/.ssh/allowed_signers
-    {{/if}}
+{{#if (eq dotter.os "windows")}}
+echo "Deployed on Windows: {{doter.packages}}"
+{{else}}
+echo "Deployed on Linux: {{#each dotter.packages}}{{#if this}}{{@key}} {{/if}}{{/each}}"
 {{/if}}
