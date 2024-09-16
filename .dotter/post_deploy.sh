1278e4965fc3901a5508b48ca0b2bf16c7138463
jack-mil <62065280+jack-mil@users.noreply.github.com>
2024-09-16 12:29:51 -0400

    actually fix post deploy and glow on windows

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -1,5 +1,6 @@
 {{#if (eq dotter.os "windows")}}
-echo "Deployed on Windows: {{{#each dotter.packages}}{{#if this}}{{@key}} {{/if}}{{/each}}"
+@echo off
+echo "Deployed on Windows: {{#each dotter.packages}}{{#if this}}{{@key}} {{/if}}{{/each}}"
 {{else}}
 echo "Deployed on Linux: {{#each dotter.packages}}{{#if this}}{{@key}} {{/if}}{{/each}}"
 {{/if}}
