52874dacd1a32c95dd4cc2d0bbfecc664a9da433
jack-mil <62065280+jack-mil@users.noreply.github.com>
2024-10-01 11:20:05 -0400

    nvim: configure lualine components

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,7 +8,29 @@ return {
         options = {
             theme = 'catppuccin',
             disabled_filetypes = { 'dashboard' },
+            section_separators = { left = '', right = '' },
         },
+        sections = {
+          lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+
+          lualine_x = {
+            { 'encoding',
+              show_bomb = true
+            },
+            { 'fileformat',
+              symbols = {
+                unix = 'LF',
+                dos  = 'CRLF',
+                mac  = 'CR'
+              }
+            },
+            'filetype'
+          },
+          lualine_y = { 'progress', 'selectioncount' },
+          lualine_z = { { 'location', separator = { right = '' }, left_padding = 2 } },
+        }
     },
 }
 
