d6d0d4aa0f873aa74e2e6c45aa6a0baf35a947aa
jack-mil <62065280+jack-mil@users.noreply.github.com>
2025-04-08 15:50:26 -0400

    yazi: update config and use new v25.4.8 features

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,4 +1,34 @@
+-- https://github.com/yazi-rs/plugins/tree/main/full-border.yazi
 require('full-border'):setup({
   -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
   type = ui.Border.ROUNDED,
 })
+
+-- In neovim, hide preview when opened
+-- https://github.com/yazi-rs/plugins/tree/main/toggle-pane.yazi
+if os.getenv('NVIM') then require('toggle-pane'):entry('min-preview') end
+
+-- https://yazi-rs.github.io/docs/tips#symlink-in-status
+Status:children_add(function(self)
+  local h = self._current.hovered
+  if h and h.link_to then
+    return ' -> ' .. tostring(h.link_to)
+  else
+    return ''
+  end
+end, 3300, Status.LEFT)
+
+-- https://yazi-rs.github.io/docs/tips#user-group-in-status
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h == nil or ya.target_family() ~= "unix" then -- linux only
+		return ""
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	}
+end, 500, Status.RIGHT)
