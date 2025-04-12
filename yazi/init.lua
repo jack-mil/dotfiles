a4b202920481f69e4cc37884bbcd171451912781
jack-mil <62065280+jack-mil@users.noreply.github.com>
2025-04-12 17:00:59 -0400

    nvim: enable ctp native-lsp integration

--- yazi/init.lua
+++ yazi/init.lua
@@ -20,15 +20,15 @@ end, 3300, Status.LEFT)
 
 -- https://yazi-rs.github.io/docs/tips#user-group-in-status
 Status:children_add(function(self)
-	local h = self._current.hovered
-	if h == nil or ya.target_family() ~= "unix" then -- linux only
-		return ""
-	end
+  local h = self._current.hovered
+  if h == nil or ya.target_family() ~= 'unix' then -- linux only
+    return ''
+  end
 
-	return ui.Line {
-		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-		":",
-		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
-		" ",
-	}
+  return ui.Line({
+    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg('magenta'),
+    ':',
+    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg('magenta'),
+    ' ',
+  })
 end, 500, Status.RIGHT)
