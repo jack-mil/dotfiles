c265c6fc8627dc0a71b0bbacefdd795d6e30af91
jack-mil <62065280+jack-mil@users.noreply.github.com>
2025-04-16 20:13:44 -0400

    bat: update themes and automate

---
+++ bat/update-themes.bash
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Author: jack-mil
+# License: BSD Zero Clause (0BSD)
+
+# This can clobber existing files if curl fails
+# Only use this on version controlled and check-in file.
+
+DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+theme_dir="${DIR}/themes"
+mkdir -p "$theme_dir"
+
+# block comment format for xml
+COPEN="<!--"
+CCLOSE="-->"
+
+CURLARGS="--silent --show-error --fail --location"
+
+SOURCE="https://github.com/catppuccin/bat"
+LICENSE=$(curl $CURLARGS "https://github.com/catppuccin/bat/raw/main/LICENSE")
+
+files=(
+    'https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme'
+    'https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme'
+    'https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme'
+    'https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme'
+)
+
+# https://github.com/SixArm/urldecode.sh/blob/0b42f15475e64daf0aee9df81b7b42089373550c/urldecode.sh
+# modified to decode just the base filename
+url_file() {
+    # url_file <string>
+
+    local url_encoded="${1//+/ }"
+    local file="${url_encoded##*/}"
+    printf '%b' "${file//%/\\x}"
+}
+
+download_list() {
+    local -n list=$1
+    local dir=$2
+    for url in "${list[@]}"; do
+        local filename=$( url_file $url )
+        echo "Downloading => $dir/$filename"
+        {
+            echo "$COPEN"
+            echo From: "$SOURCE"
+            echo "$LICENSE"
+            echo "$CCLOSE"
+            curl $CURLARGS "$url"
+        } > "${dir}/${filename}"
+    done
+}
+
+download_list files $theme_dir
