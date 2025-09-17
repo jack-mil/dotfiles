1153181fdf972eea066a42a6d54955a36a58c41f
jack-mil <62065280+jack-mil@users.noreply.github.com>
2025-09-17 09:14:57 -0400

    niri: display tweaks

--- bash/bashrc.d/03_aliases.sh
+++ bash/bashrc.d/03_aliases.sh
@@ -37,7 +37,7 @@ exists bat && alias cat='bat --color always --plain'
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias alert='notify-send --urgency=critical -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # Onefetch default args
 exists onefetch && alias onefetch='onefetch --nerd-fonts --true-color=never --iso-time'
