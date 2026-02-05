19707e98519b249d14093e9440a97d47526e1c56
jack-mil <62065280+jack-mil@users.noreply.github.com>
2026-02-05 11:14:29 -0500

    nvim: overhaul treesitter + snacks usage
    eventually can probably remove the fzf plugin in-place of snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,27 +1,110 @@
 return {
-  'folke/snacks.nvim',
-  lazy = false,
-  priority = 1000,
-  opts = {
-    ---@type snacks.picker.Config
-    explorer = {},
-    picker = {
-      sources = {
-        explorer = {
-          layout = {
-            layout = { position = 'right' },
+  {
+    'folke/snacks.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      bigfile = { enabled = true },
+      explorer = { enabled = true },
+      indent = { enabled = true, only_current = true, only_scope = true },
+      input = { enabled = true },
+      notifier = { enabled = true, timeout = 5000 },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      words = { enabled = true },
+      scroll = { enabled = true },
+      image = { enabled = true, inline = false, },
+      picker = {
+        enabled = true,
+        sources = {
+          explorer = {
+            layout = {
+              layout = { position = 'right' },
+            },
           },
         },
       },
+      dashboard = {
+        enabled = true,
+        sections = {
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
+        },
+      },
     },
+    keys = {
+      { '<leader>e', function()
+          require('snacks').explorer()
+        end,
+        desc = 'Explorer Snacks (cwd)',
+      },
+      { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+      { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Some globals for debugging (lazy-loaded)
+          _G.dd = function(...)
+            Snacks.debug.inspect(...)
+          end
+          _G.bt = function()
+            Snacks.debug.backtrace()
+          end
+          -- Override print to use Snacks pretty print for `:=` command
+
+          if vim.fn.has("nvim-0.11") == 1 then
+            vim._print = function(_, ...)
+              dd(...)
+            end
+          else
+            vim.print = _G.dd
+          end
+
+          -- Some toggle mappings
+          Snacks.toggle.treesitter():map("<leader>uT")
+          Snacks.toggle.inlay_hints():map("<leader>uh")
+          Snacks.toggle.indent():map("<leader>ug")
+          Snacks.toggle.dim():map("<leader>uD")
+        end,
+      })
+    end
   },
-  keys = {
-    {
-      '<leader>e',
-      function()
-        require('snacks').explorer()
-      end,
-      desc = 'Explorer Snacks (cwd)',
+  -- Session management. Saves session in the background,
+  -- Can also restore sessions when returning through the dashboard.
+  {
+    'folke/persistence.nvim',
+    event = 'BufReadPre', -- this will only start session saving when an actual file was opened
+    opts = {},
+  },
+  -- Configure LuaLS to support auto-completion and type checking
+  -- when editing configuration.
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    cmd = "LazyDev",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "snacks.nvim", words = { "Snacks" } },
+      },
     },
   },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,20 @@
+-- memoize installed capabilities
+local installed = {}
+local function get_lang(lang)
+  return installed[lang]
+end
+local _queries = {}
+local function have_query(lang, query)
+  if not get_lang(lang) then
+    return
+  end
+  local key = lang .. ':' .. query
+  if _queries[key] == nil then
+    _queries[key] = vim.treesitter.query.get(lang, query) ~= nil
+  end
+  return _queries[key]
+end
+
 return {
   {
     'nvim-treesitter/nvim-treesitter-context',
@@ -12,36 +29,23 @@ return {
     branch = 'main',
     version = false,
     build = ':TSUpdate',
-    event = { 'VeryLazy' },
+    lazy = false,
     cmd = { 'TSUpdate', 'TSInstall', 'TSLog', 'TSUninstall' },
     config = function(...)
+      -- Create map of installed languges
       local TS = require('nvim-treesitter')
-      -- .setup() not required unless overriding `install_dir`
-      -- TS.setup(opts)
-
-      -- memoize installed capabilities
-      local installed = {}
       for _, lang in ipairs(TS.get_installed('parsers')) do
         installed[lang] = true
       end
 
-      local _queries = {}
-      local function have_query(lang, query)
-        local key = lang .. ':' .. query
-        if _queries[key] == nil then
-          _queries[key] = vim.treesitter.query.get(lang, query) ~= nil
-        end
-        return _queries[key]
-      end
-
       -- enable treesitter autocmd if language is installed (manually with `:TSInstall`)
       vim.api.nvim_create_autocmd('FileType', {
-        group = vim.api.nvim_create_augroup('treesitter', { clear = true }),
+        group = vim.api.nvim_create_augroup('enable-treesitter', { clear = true }),
         callback = function(ev)
           local lang = vim.treesitter.language.get_lang(ev.match)
 
           -- skip enabling if parser is not installed for this language
-          if lang == nil or installed[lang] == nil then
+          if lang == nil or not get_lang(lang) then
             return
           end
 
@@ -64,4 +68,67 @@ return {
       }) -- end AutoCMD
     end  -- config()
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    event = "VeryLazy",
+    opts = {
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        -- Helper configured in config() below to create buffer-local keymaps
+        keys = {
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+        },
+      },
+    },
+    config = function(_, opts)
+      local TS = require("nvim-treesitter-textobjects")
+      TS.setup(opts)
+
+      local function attach(buf)
+        local ft = vim.bo[buf].filetype
+        if not (vim.tbl_get(opts, "move", "enable") and have_query(ft, "textobjects")) then
+          return
+        end
+        ---@type table<string, table<string, string>>
+        local moves = vim.tbl_get(opts, "move", "keys") or {}
+
+        for method, keymaps in pairs(moves) do
+          for key, query in pairs(keymaps) do
+            local queries = type(query) == "table" and query or { query }
+            local parts = {}
+            for _, q in ipairs(queries) do
+              local part = q:gsub("@", ""):gsub("%..*", "")
+              part = part:sub(1, 1):upper() .. part:sub(2)
+              table.insert(parts, part)
+            end
+            local desc = table.concat(parts, " or ")
+            desc = (key:sub(1, 1) == "[" and "Prev " or "Next ") .. desc
+            desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and " End" or " Start")
+            if not (vim.wo.diff and key:find("[cC]")) then
+              vim.keymap.set({ "n", "x", "o" }, key, function()
+                require("nvim-treesitter-textobjects.move")[method](query, "textobjects")
+              end, {
+                buffer = buf,
+                desc = desc,
+                silent = true,
+              })
+            end
+          end
+        end
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("lazyvim_treesitter_textobjects", { clear = true }),
+        callback = function(ev)
+          attach(ev.buf)
+        end,
+      })
+      vim.tbl_map(attach, vim.api.nvim_list_bufs())
+    end,
+  }
 }
